66abdf74358b458e731eb5adb895b09cd35fe2e6
ProgrammerWithoutaName <Talk@ProgrammerWithoutA.Name>
2016-03-30 14:08:13 -0400

    updating readme and adding descriptions and comments to example scriptDefinitions.js

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ Each Angular module will get broken up into a module definition, a module export
 The end solution will most likely not compile- this is mostly a proof of concept project. It will, however, get you fairly close. Since this is more proof of concept, there isn't an interface, to test this out on your project, modify index.js imported scripts and directories.
 
 the output will be put into an output folder- it will just be the equivelent app folder, and not run by itself. From there, you will need to use the framework of choice to enable ES2016 for your site.
+
+
+How to run:
+Create a ScriptDefinition. This will define your project and the external modules your project pulls in. There is an example script definition in the project (scriptDefinition.js).
+if you are running this project from this directory, the command to run it would be
+(assuming that scriptDefinitions is located in C:\Development\ng-nuke\scriptDefinitions.js)
+node index.js C:\Development\ng-nuke\scriptDefinitions.js
--- scriptDefinitions.js
+++ scriptDefinitions.js
@@ -12,18 +12,29 @@ let importedScripts = [
     "C:/Development/refactoring-angular/bower_components/angular-sanitize/angular-sanitize.js",
     "C:/Development/refactoring-angular/bower_components/angular-ui-router/release/angular-ui-router.js"
 ];
+
 let directories = [
     {
         individualScripts: true,
         filePaths: importedScripts,
         tags: {
+            // convert: if true, then the module and components will be broken out, otherwise it will create a moduleExport
+            // in the importFix folder
             convert: false,
+            // singleFile defines if we are converting single files or entire directories.
             singleFile: true,
+            // useful for tagging if the scripts are part of the project or not. Don't do anything with the tags at the moment.
             vendor: true,
+            // this will collapse the modules into a single export module. In the example of ui.router: ui.router has no components defined, but
+            // has a number of dependencies that contain the components like $stateProvider and $urlRouterProvider
             collapse: ['ui.router', 'ngAnimate', 'ngSanitize'],
+            // folder that everything is based off of. if baseLocation is output, it will base all of the require's off of where things should fall in output/src
             baseFolder: 'src',
+            // where to put the scripts given. In the case of single files, folder structure is not maintained, so all the files would go to output/src/importFix
             relativeRemap: 'src/importFix',
+            // where the files are based to begin with. Used for determining filelocations for import statements
             oldBase: 'C:/Development/refactoring-angular/',
+            // new baseLocation of the files, so if "output" is defined, everything should be place into the output directory
             newBase: baseLocation
         }
     },
@@ -33,6 +44,7 @@ let directories = [
         tags: {
             web: true,
             convert: true,
+            //trackNonScriptFiles will copy all non-script files to their new relative location. It will also move .spec.js files as if they are non script files.
             trackNonScriptFiles: true,
             baseFolder: 'src',
             relativeRemap: 'src',
@@ -48,8 +60,11 @@ let preferences = {
 
 module.exports =
 {
+    //Code Style Preferences. Right now the only thing we use is indent, more to come in the future.
     preferences,
+    // scripts and directories that are to be converted, or have an importFix file added in the case of vendor scripts.
     directories,
+    // Globals you use in your script that aren't Angular. They must be defined here or the script will not work.
     definedGlobals: [
         'malarkey',
         'moment',
